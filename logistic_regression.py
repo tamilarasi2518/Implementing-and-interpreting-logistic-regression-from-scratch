632307844a0bfcc5a18d1c940a04e996a1a30a15
tamilarasi2518 <tamilarasi2518@gmail.com>
2025-11-25 16:59:06 +0530

    Add files via upload

---
+++ logistic_regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class LogisticRegressionScratch:
+    def __init__(self, lr=0.01, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear = np.dot(X, self.weights) + self.bias
+            y_pred = self.sigmoid(linear)
+
+            dw = (1/n_samples) * np.dot(X.T, (y_pred - y))
+            db = (1/n_samples) * np.sum(y_pred - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        return (self.sigmoid(np.dot(X, self.weights) + self.bias) >= 0.5).astype(int)
